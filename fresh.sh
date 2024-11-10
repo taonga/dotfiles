49e0ad80306a428412f528d17da5c00a35ce960b
Anthony Sexton <anthonysexton@gmail.com>
2024-11-10 11:57:14 +1000

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -45,7 +45,7 @@ mkdir $HOME/Herd
 # ./clone.sh
 
 # Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source ./.macos
